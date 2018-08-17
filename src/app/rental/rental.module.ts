be9a1ff58a9075d125f4ebc63e12fa458e71265b
jovan <jovanj85@yahoo.com>
2018-08-17 10:38:50 +0200

    Adds udate featere, update page, new rental guard

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -5,7 +5,8 @@ import { Daterangepicker } from 'ng2-daterangepicker';
 import { FormsModule } from '@angular/forms';
 
 import { NgModule } from '@angular/core';
-import {NgPipesModule} from 'ngx-pipes';
+import { EditableModule } from '../common/components/editable/editable.module';
+import { NgPipesModule, UcWordsPipe } from 'ngx-pipes';
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
 import { RentalComponent } from './rental.component';
@@ -14,9 +15,11 @@ import { RentlDetailComponent } from './rentl-detail/rentl-detail.component';
 import { UppercasePipe } from '../common/pipes/uppercase.pipe';
 import { MapModule } from '../common/map/map.module';
 import { AuthGuard } from '../auth/shared/auth.guard';
+import { RentalGuard } from './shared/rental.guard';
 import { RentalDetailBookingComponent } from './rentl-detail/rental-detail-booking/rental-detail-booking.component';
 import { RentalSearchComponent } from './rental-search/rental-search.component';
 import { RentalCreateComponent } from './rental-create/rental-create.component';
+import { RentalUpdateComponent } from './rental-update/rental-update.component';
 
 const routes: Routes = [
   { path: 'rentals',
@@ -25,6 +28,7 @@ const routes: Routes = [
       { path: '', component: RentalListComponent },
       { path: 'new', component: RentalCreateComponent, canActivate: [AuthGuard] },
       { path: ':rentlId', component: RentlDetailComponent },
+      { path: ':rentalId/edit', component: RentalUpdateComponent, canActivate: [AuthGuard, RentalGuard] },
       { path: ':city/homes', component: RentalSearchComponent }
     ]
   }
@@ -39,7 +43,8 @@ const routes: Routes = [
     UppercasePipe,
     RentalDetailBookingComponent,
     RentalSearchComponent,
-    RentalCreateComponent
+    RentalCreateComponent,
+    RentalUpdateComponent
   ],
   imports: [
     BrowserModule,
@@ -49,7 +54,12 @@ const routes: Routes = [
     MapModule,
     Daterangepicker,
     FormsModule,
+    EditableModule,
     RouterModule.forChild(routes)
+  ],
+  providers: [
+    UcWordsPipe,
+    RentalGuard
   ]
 })
 export class RentalModule {
